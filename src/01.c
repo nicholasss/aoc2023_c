1c9afc341b542661dcabdce5fcf578b6085e8121
Nicholas <nicholasss@users.noreply.github.com>
2025-07-08 16:18:20 -0400

    style: formatted files

--- src/01.c
+++ src/01.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
-	char number;
-	int start_pos;
+  char number;
+  int start_pos;
 } num_obj;
 
 // function signatures
@@ -20,166 +20,152 @@ num_obj *string_to_num(int start_pos, char *str);
 // main function
 int main() {
 
-	const char *test_file = "./source/01/test.txt";
-	const char *main_file = "./source/01/main.txt";
+  const char *test_file = "./src/01/test.txt";
+  const char *main_file = "./src/01/main.txt";
 
-	int test_num = read_txt(test_file);
-	int main_num = read_txt(main_file);
+  int test_num = read_txt(test_file);
+  int main_num = read_txt(main_file);
 
-	printf("Test calculation is %d\n", test_num);
-	printf("Main calculation is %d\n", main_num);
+  printf("Test calculation is %d\n", test_num);
+  printf("Main calculation is %d\n", main_num);
 }
 
-void free_obj(num_obj *obj) {
-	free(obj);
-}
+void free_obj(num_obj *obj) { free(obj); }
 
 num_obj *make_obj(int start_pos, char number) {
-	num_obj *new_obj = malloc(sizeof(num_obj));
-	if (new_obj == NULL) {
-		printf("Unable to allocate memory for new object\n");
-		return NULL;
-	}
+  num_obj *new_obj = malloc(sizeof(num_obj));
+  if (new_obj == NULL) {
+    printf("Unable to allocate memory for new object\n");
+    return NULL;
+  }
 
-	new_obj->start_pos = start_pos;
-	new_obj->number = number;
-	return new_obj;
+  new_obj->start_pos = start_pos;
+  new_obj->number = number;
+  return new_obj;
 };
 
 // function definitions
 num_obj *string_to_num(int start_pos, char *str) {
-	if (strcmp(str, "one") == 0) {
-		return make_obj(start_pos, '1');
-	} else if (strcmp(str, "two") == 0) {
-		return make_obj(start_pos, '2');
-	} else if (strcmp(str, "three") == 0) {
-		return make_obj(start_pos, '3');
-	} else if (strcmp(str, "four") == 0) {
-		return make_obj(start_pos,'4');
-	} else if (strcmp(str, "five") == 0) {
-		return make_obj(start_pos, '5');
-	} else if (strcmp(str, "six") == 0) {
-		return make_obj(start_pos, '6');
-	} else if (strcmp(str, "seven") == 0) {
-		return make_obj(start_pos, '7');
-	} else if (strcmp(str, "eight") == 0) {
-		return make_obj(start_pos, '8');
-	} else if (strcmp(str, "nine") == 0) {
-		return make_obj(start_pos, '9');
-	} else {
-		printf("Unable to identify the following number: %s\n", str); 
-		return NULL;
-	}
+  if (strcmp(str, "one") == 0) {
+    return make_obj(start_pos, '1');
+  } else if (strcmp(str, "two") == 0) {
+    return make_obj(start_pos, '2');
+  } else if (strcmp(str, "three") == 0) {
+    return make_obj(start_pos, '3');
+  } else if (strcmp(str, "four") == 0) {
+    return make_obj(start_pos, '4');
+  } else if (strcmp(str, "five") == 0) {
+    return make_obj(start_pos, '5');
+  } else if (strcmp(str, "six") == 0) {
+    return make_obj(start_pos, '6');
+  } else if (strcmp(str, "seven") == 0) {
+    return make_obj(start_pos, '7');
+  } else if (strcmp(str, "eight") == 0) {
+    return make_obj(start_pos, '8');
+  } else if (strcmp(str, "nine") == 0) {
+    return make_obj(start_pos, '9');
+  } else {
+    printf("Unable to identify the following number: %s\n", str);
+    return NULL;
+  }
 }
 
 num_obj **read_line(char *line) {
-	// returns two num_objs
-	num_obj *first_digit;
-	num_obj *first_spelled;
-	num_obj *second_digit;
-	num_obj *second_spelled;
-	int line_len = (int)strlen(line);
+  // returns two num_objs
+  num_obj *first_digit;
+  num_obj *first_spelled;
+  num_obj *second_digit;
+  num_obj *second_spelled;
+  int line_len = (int)strlen(line);
 
-	// first letter
-	// find both first letter and first number then return both
-	for (int i = 0; i < line_len; i++) {
-		if (isalpha(line[i])) {
-			char *buffer = malloc(sizeof(char) * 10);
+  // first letter
+  // find both first letter and first number then return both
+  for (int i = 0; i < line_len; i++) {
+    if (isalpha(line[i])) {
+      char *buffer = malloc(sizeof(char) * 10);
 
-			for (int j = i; j < line_len; j++) {
-				strncpy(buffer, line + i, i - j);
-				if (isdigit(line[j])) {
-					break;
-				}
+      for (int j = i; j < line_len; j++) {
+        strncpy(buffer, line + i, i - j);
+        if (isdigit(line[j])) {
+          break;
+        }
 
-				first_spelled = string_to_num(i, buffer);
+        first_spelled = string_to_num(i, buffer);
 
-				if (first_spelled != NULL) {
-				
-				}
-			}
-		}
-	}
-
+        if (first_spelled != NULL) {
+        }
+      }
+    }
+  }
 }
 
 int read_txt(const char *file_path) {
-	char *numbers[] = {
-		"zero",
-		"one",
-		"two",
-		"three",
-		"four",
-		"five",
-		"six",
-		"seven",
-		"eight",
-		"nine"
-	};
+  char *numbers[] = {"zero", "one", "two",   "three", "four",
+                     "five", "six", "seven", "eight", "nine"};
 
-	FILE *file = fopen(file_path, "r");
+  FILE *file = fopen(file_path, "r");
 
-	if (file == NULL) {
-		printf("Unable to open file: %s\n", file_path);
-		return -1;
-	}
+  if (file == NULL) {
+    printf("Unable to open file: %s\n", file_path);
+    return -1;
+  }
 
-	char line[256]; // buffer for each line of the 
-	int sum = 0;
+  char line[256]; // buffer for each line of the
+  int sum = 0;
 
-	// TODO: add spelled out digits
-	while(fgets(line, sizeof(line), file)) {
-		num_obj *first; 
-		num_obj *second;
+  // TODO: add spelled out digits
+  while (fgets(line, sizeof(line), file)) {
+    num_obj *first;
+    num_obj *second;
 
-		int line_len = (int)strlen(line);
+    int line_len = (int)strlen(line);
 
-		// get first digit number
-		for (int i = 0; i < line_len; i++) {
-			char nchar = line[i];
-			if (isdigit(nchar)) {
-				first = nchar;
-				break;
-			}
-		}
+    // get first digit number
+    for (int i = 0; i < line_len; i++) {
+      char nchar = line[i];
+      if (isdigit(nchar)) {
+        first = nchar;
+        break;
+      }
+    }
 
-		// check 5 letters for spelled number
-		char letters[6];
-		for (int i = 0; i < 5; i++) {
-			letters[i] = line[i];
-			// first = string_to_num(letters);
-		}
+    // check 5 letters for spelled number
+    char letters[6];
+    for (int i = 0; i < 5; i++) {
+      letters[i] = line[i];
+      // first = string_to_num(letters);
+    }
 
-		// get second number
-		char last_char = line[0];
-		for (int i = 0; i < line_len; i++) {
-			char curr_char = line[i];
+    // get second number
+    char last_char = line[0];
+    for (int i = 0; i < line_len; i++) {
+      char curr_char = line[i];
 
-			if (isdigit(curr_char)) {
-				last_char = curr_char;
-			}
-		}
-		second = last_char;
+      if (isdigit(curr_char)) {
+        last_char = curr_char;
+      }
+    }
+    second = last_char;
 
-		// check for the last letters
+    // check for the last letters
 
-		// combine them together #1,#2
-		char cnumber[10] = "00";
-		cnumber[0] = first;
-		cnumber[1] = second;
-		int number = atoi(cnumber);
+    // combine them together #1,#2
+    char cnumber[10] = "00";
+    cnumber[0] = first;
+    cnumber[1] = second;
+    int number = atoi(cnumber);
 
-		if (number == 0) {
-			printf("Unable to convert %s to integer\n", cnumber);
-			return -1;
-		}
+    if (number == 0) {
+      printf("Unable to convert %s to integer\n", cnumber);
+      return -1;
+    }
 
-		// add to sum var
-		sum += number;
-	}
+    // add to sum var
+    sum += number;
+  }
 
-	fclose(file);
+  fclose(file);
 
-	printf("%d is the sum of the digits.\n", sum);
-	return sum;
+  printf("%d is the sum of the digits.\n", sum);
+  return sum;
 }
--- src/02.c
+++ src/02.c
@@ -1,12 +1,12 @@
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
 
 // constants and structs
-const char *test_path = "/Users/nicholas/Developer/aoc2023_c/source/02/test.txt";
-const char *main_path = "/Users/nicholas/Developer/aoc2023_c/source/02/main.txt";
+const char *test_path = "./src/02/test.txt";
+const char *main_path = "./src/02/main.txt";
 
 const int num_red = 12;
 const int num_green = 13;
@@ -16,181 +16,182 @@ const char comma = ',';
 const char semicolon = ';';
 
 typedef struct {
-	int num;
-	char *color;
+  int num;
+  char *color;
 } color_num;
 
 // function signatures
-void free_obj_list(color_num** list, int list_length);
-bool add_to_struct_list(color_num** list, int arr_length, color_num *item);
+void free_obj_list(color_num **list, int list_length);
+bool add_to_struct_list(color_num **list, int arr_length, color_num *item);
 void free_obj(color_num *obj);
 color_num *text_to_obj(char *str);
 int read_txt(const char *file_path);
 
 // main
 int main() {
-	int test = read_txt(test_path);
-	int main = read_txt(main_path);
+  int test = read_txt(test_path);
+  int main = read_txt(main_path);
 
-	printf("Test calculation is %d\n", test);
-	printf("Main calculation is %d\n", main);
+  printf("Test calculation is %d\n", test);
+  printf("Main calculation is %d\n", main);
 }
 
 // function definitions
 // free obj
 void free_obj(color_num *obj) {
-	free(obj->color);
-	free(obj);
+  free(obj->color);
+  free(obj);
 }
 
 // free object list
-void free_obj_list(color_num** list, int list_length) {
-	for (int i = 0; i < list_length; i++) {
-		if (list[i] != NULL) {
-			free_obj(list[i]);
-		}
-	}
-	free(list);
+void free_obj_list(color_num **list, int list_length) {
+  for (int i = 0; i < list_length; i++) {
+    if (list[i] != NULL) {
+      free_obj(list[i]);
+    }
+  }
+  free(list);
 }
 
 // adding to object list
-bool add_to_struct_list(color_num** list, int list_length, color_num *p_item) {
-	bool success = false;
+bool add_to_struct_list(color_num **list, int list_length, color_num *p_item) {
+  bool success = false;
 
-	for (int i = 0; i < list_length; i++) {
+  for (int i = 0; i < list_length; i++) {
 
-		if (list[i] != NULL) {
-			continue;
-		} else {
-			list[i] = p_item;
-			success = true;
-			break;
-		}
+    if (list[i] != NULL) {
+      continue;
+    } else {
+      list[i] = p_item;
+      success = true;
+      break;
+    }
 
-		if (i == list_length - 1) {
-			printf("Unable to add to array, array too small.\n");
-			return false;
-		}
-	}
-	return success;
+    if (i == list_length - 1) {
+      printf("Unable to add to array, array too small.\n");
+      return false;
+    }
+  }
+  return success;
 }
 
 // create obj
 color_num *text_to_obj(char *str) {
-	// printf("'%s'", str);
+  // printf("'%s'", str);
 
-	color_num *obj = malloc(sizeof(color_num));
-	char number[3];
+  color_num *obj = malloc(sizeof(color_num));
+  char number[3];
 
-	// i.e. 3 green, number then color
-	int num_digits = 0;
-	char second_digit = str[1];
-	if (isdigit(second_digit)) {
-		num_digits = 2;
-		strncpy(number, str, num_digits); // two digits
-	} else {
-		num_digits = 1;
-		strncpy(number, str, num_digits); // one digit
-	}
-	obj->num = atoi(number);
+  // i.e. 3 green, number then color
+  int num_digits = 0;
+  char second_digit = str[1];
+  if (isdigit(second_digit)) {
+    num_digits = 2;
+    strncpy(number, str, num_digits); // two digits
+  } else {
+    num_digits = 1;
+    strncpy(number, str, num_digits); // one digit
+  }
+  obj->num = atoi(number);
 
-	int color_starts_at = num_digits + 1;
-	char *color = malloc(10 * sizeof(char));
-	strncpy(color, str + color_starts_at, (sizeof(str) - color_starts_at) + 1);
-	obj->color = color;
-	return obj;
+  int color_starts_at = num_digits + 1;
+  char *color = malloc(10 * sizeof(char));
+  strncpy(color, str + color_starts_at, (sizeof(str) - color_starts_at) + 1);
+  obj->color = color;
+  return obj;
 }
 
 int read_txt(const char *file_path) {
-	
-	FILE *file = fopen(file_path, "r");
-	if (file == NULL) {
-		printf("Unable to open file: %s\n", file_path);
-		return -1;
-	}
 
-	int sum = 0;
-	int game_num = 0;
-	char line[256];
-	while (fgets(line, sizeof(line), file)) {
-		game_num += 1;
+  FILE *file = fopen(file_path, "r");
+  if (file == NULL) {
+    printf("Unable to open file: %s\n", file_path);
+    return -1;
+  }
 
-		int objs_list_size = 32;
-		color_num **objs = malloc(sizeof(color_num) * objs_list_size);
+  int sum = 0;
+  int game_num = 0;
+  char line[256];
+  while (fgets(line, sizeof(line), file)) {
+    game_num += 1;
 
-		int line_length = (int)strlen(line); // reconverted from unsigned long to int
-		int obj_starts_at = 0;
-		int obj_ends_at = 0;
+    int objs_list_size = 32;
+    color_num **objs = malloc(sizeof(color_num) * objs_list_size);
 
-		// read characters until there is a comma or semicolon
-		for (int i = 0; i < line_length; i++) {
-			// finds the first color-num after 'game x:'
-			if (line[i] == ':') {
-				obj_starts_at = i + 1;
-			}
+    int line_length =
+        (int)strlen(line); // reconverted from unsigned long to int
+    int obj_starts_at = 0;
+    int obj_ends_at = 0;
 
-			// finds the end of the character and uses it
-			if (line[i] == comma || line[i] == semicolon || line[i] == '\n') {
-				// why would I not also put a newline to create an object
-				obj_ends_at = i;
+    // read characters until there is a comma or semicolon
+    for (int i = 0; i < line_length; i++) {
+      // finds the first color-num after 'game x:'
+      if (line[i] == ':') {
+        obj_starts_at = i + 1;
+      }
 
-				char *score_str = malloc(sizeof(char) * 16);
-				int str_len = obj_ends_at - obj_starts_at - 1;
-				strncpy(score_str, line + obj_starts_at + 1, str_len);
+      // finds the end of the character and uses it
+      if (line[i] == comma || line[i] == semicolon || line[i] == '\n') {
+        // why would I not also put a newline to create an object
+        obj_ends_at = i;
 
-				// new_obj is created and can be added to the list
-				color_num* new_obj = text_to_obj(score_str);
-				free(score_str);
+        char *score_str = malloc(sizeof(char) * 16);
+        int str_len = obj_ends_at - obj_starts_at - 1;
+        strncpy(score_str, line + obj_starts_at + 1, str_len);
 
-				bool status = add_to_struct_list(objs, objs_list_size, new_obj);
-				if (status == false) {
-					printf("failed to add to list\n");
-					return -1;
-				}
-				obj_starts_at = obj_ends_at + 1;
-			}
-		}
-		
-		// obj list should then be added up to sum but only if its a valid game
-		// if invalid game, break to skip to next line;
-		int invalid_game = false;
-		for (int i = 0; i < objs_list_size; i++) {
-			// invalid obj pointer
-			if (objs[i] == NULL) {
-				continue;
-			}
+        // new_obj is created and can be added to the list
+        color_num *new_obj = text_to_obj(score_str);
+        free(score_str);
 
-			char *color = objs[i]->color; // this line causes segfault -- why?
-			int num = objs[i]->num;
+        bool status = add_to_struct_list(objs, objs_list_size, new_obj);
+        if (status == false) {
+          printf("failed to add to list\n");
+          return -1;
+        }
+        obj_starts_at = obj_ends_at + 1;
+      }
+    }
 
-			if (strcmp(color, "red") == 0) {
-				if (num > num_red) {
-					invalid_game = true;
-				}
-			}
-			if (strcmp(color, "blue") == 0) {
-				if (num > num_blue) {
-					invalid_game = true;
-				}
-			}
-			if (strcmp(color, "green") == 0) {
-				if (num > num_green) {
-					invalid_game = true;
-				}
-			}
-		}
+    // obj list should then be added up to sum but only if its a valid game
+    // if invalid game, break to skip to next line;
+    int invalid_game = false;
+    for (int i = 0; i < objs_list_size; i++) {
+      // invalid obj pointer
+      if (objs[i] == NULL) {
+        continue;
+      }
 
-		if (invalid_game == true) {
-			continue; // go to the next line in the test
-		} else {
-			sum += game_num;
-		}
+      char *color = objs[i]->color; // this line causes segfault -- why?
+      int num = objs[i]->num;
 
-		// freeing resources
-		// this should free all of the objs within this list
-		free_obj_list(objs, objs_list_size);
-	}
+      if (strcmp(color, "red") == 0) {
+        if (num > num_red) {
+          invalid_game = true;
+        }
+      }
+      if (strcmp(color, "blue") == 0) {
+        if (num > num_blue) {
+          invalid_game = true;
+        }
+      }
+      if (strcmp(color, "green") == 0) {
+        if (num > num_green) {
+          invalid_game = true;
+        }
+      }
+    }
 
-	fclose(file);
-	return sum;
+    if (invalid_game == true) {
+      continue; // go to the next line in the test
+    } else {
+      sum += game_num;
+    }
+
+    // freeing resources
+    // this should free all of the objs within this list
+    free_obj_list(objs, objs_list_size);
+  }
+
+  fclose(file);
+  return sum;
 }
